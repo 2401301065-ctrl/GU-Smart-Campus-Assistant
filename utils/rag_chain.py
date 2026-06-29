64e6819c741576e539eedf0dd2e02f3089af9553
Vanshika Ahlawat <ahlawatvanshikaa@gmail.com>
2026-06-29 22:26:18 +0530

    fix: added safe API key handling for Gemini

--- utils/rag_chain.py
+++ utils/rag_chain.py
@@ -1,34 +1,15 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# ---------------------------
-# LLM FUNCTION
-# ---------------------------
 def get_llm():
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not api_key:
+        raise ValueError("GOOGLE_API_KEY is missing in environment variables")
+
     llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
-        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        google_api_key=api_key,
         temperature=0.3
     )
     return llm
-
-
-# ---------------------------
-# PROMPT FUNCTION
-# ---------------------------
-def get_prompt():
-    prompt = ChatPromptTemplate.from_template("""
-You are Geeta University Assistant AI.
-
-Answer user questions ONLY using the provided context.
-
-Context:
-{context}
-
-Question:
-{question}
-
-Answer clearly and simply:
-""")
-    return prompt
