c5818721ce354ff0eed64837fe81737b71063bc3
Vanshika Ahlawat <ahlawatvanshikaa@gmail.com>
2026-06-29 22:19:57 +0530

    fix rag_chain import issue

--- utils/rag_chain.py
+++ utils/rag_chain.py
@@ -1,13 +1,34 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 
+# ---------------------------
+# LLM FUNCTION
+# ---------------------------
+def get_llm():
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0.3
+    )
+    return llm
+
+
+# ---------------------------
+# PROMPT FUNCTION
+# ---------------------------
 def get_prompt():
     prompt = ChatPromptTemplate.from_template("""
-    You are Geeta University Assistant AI.
-    Answer user questions based only on provided context.
+You are Geeta University Assistant AI.
 
-    Context: {context}
-    Question: {question}
+Answer user questions ONLY using the provided context.
 
-    Answer:
-    """)
+Context:
+{context}
+
+Question:
+{question}
+
+Answer clearly and simply:
+""")
     return prompt
